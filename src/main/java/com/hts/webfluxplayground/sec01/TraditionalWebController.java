d27608855c1916a0a7865ebe36b35a684ab7964d
Moses Hunsu <hunsumoses7@gmail.com>
2025-02-02 01:27:06 +0100

    created traditional web controller class

---
+++ src/main/java/com/hts/webfluxplayground/sec01/TraditionalWebController.java
@@ -0,0 +1,34 @@
+package com.hts.webfluxplayground.sec01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("traditional")
+@Slf4j
+public class TraditionalWebController {
+
+    private final RestClient restClient = RestClient.builder()
+                                                    .requestFactory(new JdkClientHttpRequestFactory())
+                                                    .baseUrl("http://localhost:7080")
+                                                    .build();
+
+    @GetMapping("products")
+    public List<Product> getProducts() {
+        var list = this.restClient.get()
+                                    .uri("/demo01/products")
+                                    .retrieve()
+                                    .body(new ParameterizedTypeReference<List<Product>>() {
+                });
+        log.info("received response: {}", list);
+        return list;
+    }
+
+}
