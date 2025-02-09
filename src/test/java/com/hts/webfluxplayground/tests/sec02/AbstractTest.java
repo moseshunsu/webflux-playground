a8e3c91d9f2533c6d062a2671e5e73bcbca17bc2
Moses Hunsu <hunsumoses7@gmail.com>
2025-02-09 17:42:22 +0100

    wrote how to show sql

--- src/test/java/com/hts/webfluxplayground/tests/sec02/AbstractTest.java
+++ src/test/java/com/hts/webfluxplayground/tests/sec02/AbstractTest.java
@@ -2,6 +2,10 @@ package com.hts.webfluxplayground.tests.sec02;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "sec=sec02")
+@SpringBootTest(properties = {
+        "sec=sec02",
+        "logging.level.org.springframework.r2dbc=TRACE"
+
+})
 public abstract class AbstractTest {
 }
