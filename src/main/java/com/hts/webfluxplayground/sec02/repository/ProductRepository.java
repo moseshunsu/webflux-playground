80ce782868374aeb66abce0f0373008e3326433d
Moses Hunsu <hunsumoses7@gmail.com>
2025-02-09 18:07:00 +0100

    implemented pageable

--- src/main/java/com/hts/webfluxplayground/sec02/repository/ProductRepository.java
+++ src/main/java/com/hts/webfluxplayground/sec02/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.hts.webfluxplayground.sec02.repository;
 
 import com.hts.webfluxplayground.sec02.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,5 +10,7 @@ import reactor.core.publisher.Flux;
 public interface ProductRepository extends ReactiveCrudRepository<Product, Integer> {
 
     Flux<Product> findByPriceBetween(int from, int to);
+
+    Flux<Product> findBy(Pageable pageable);
 
 }
